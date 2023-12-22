0af7cf4e140b77c630b939b71b4f92a94e9c1f0c
skyemperor611 <blackmonarch124@gmail.com>
2023-12-22 21:11:39 +0900

    first commit

---
+++ src/test/java/org/zerock/mallapi/service/TodoServiceTest.java
@@ -0,0 +1,72 @@
+package org.zerock.mallapi.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mallapi.dto.PageRequestDTO;
+import org.zerock.mallapi.dto.PageResponseDTO;
+import org.zerock.mallapi.dto.TodoDTO;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@Log4j2
+class TodoServiceTest {
+
+    @Autowired
+    private TodoService todoService;
+
+    @Test
+    public void testRegister() {
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title("서비스 테스트")
+                .writer("tester")
+                .dueDate(LocalDate.of(2023,10,10))
+                .build();
+
+        Long tno = todoService.register(todoDTO);
+
+        log.info("TNO: " + tno);
+    }
+
+    @Test
+    public void testGet() {
+        Long tno = 101L;
+
+        TodoDTO todoDTO = todoService.get(tno);
+
+        log.info(todoDTO);
+    }
+
+    @Test
+    public void testModify() {
+        TodoDTO todoDTO = TodoDTO.builder()
+                .tno(100L)
+                .title("서비스 테스트 수정")
+                .writer("tester")
+                .dueDate(LocalDate.of(2023,10,10))
+                .build();
+
+        todoService.modify(todoDTO);
+    }
+
+    @Test
+    public void testRemove() {
+        Long tno = 100L;
+
+        todoService.remove(tno);
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResponseDTO<TodoDTO> response = todoService.list(pageRequestDTO);
+
+        log.info(response);
+    }
+}
